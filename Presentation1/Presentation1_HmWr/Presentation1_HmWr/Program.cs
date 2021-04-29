9a33e2b7dd90921521e37888ac05eb02798e613b
TarasHrysiukk <64036257+TarasHrysiukk@users.noreply.github.com>
2021-04-29 14:56:58 +0300

    Task1_Presentatin1_done

--- Presentation1/Presentation1_HmWr/Presentation1_HmWr/Program.cs
+++ Presentation1/Presentation1_HmWr/Presentation1_HmWr/Program.cs
@@ -6,20 +6,7 @@ namespace Presentation1_HmWr
     {
         static void Main(string[] args)
         {
-            string x = Console.ReadLine();
-            int a = Convert.ToInt32(x);
-            switch(a)
-            {
-                case 1:
-                    Console.WriteLine("один без брейк");
-                    break;
-                case 2:
-                    Console.WriteLine("два з брейк");
-                    break;
-                case 3:
-                    Console.WriteLine("два з брейк");
-                    break;
-            }
+           
         }
     }
 }
--- Presentation1/Presentation1_Task/Presentation1_Task/Program.cs
+++ Presentation1/Presentation1_Task/Presentation1_Task/Program.cs
@@ -6,7 +6,60 @@ namespace Presentation1_Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Task1.1
+            //Define integer variables a and b.Read values a and b from
+            //Console and calculate: a+b, a-b, a*b, a/b. Output obtained results.
+            int a;
+            int b;
+            Console.WriteLine("Enter value of a:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter value of b (b<a):");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a + b;
+            Console.WriteLine("Sum a + b = " + sum);
+
+            int subtraction = a - b;
+            Console.WriteLine("Subtraction a - b = " + subtraction);
+
+            int multiply = a * b;
+            Console.WriteLine("Multiply a * b = " + multiply);
+
+            int divide = a / b;
+            Console.WriteLine("Divide a / b = " + divide);
+
+            //Task 1.2
+            //Output question “How are you ?“. Define string variable answer.
+            //Read the value answer and output: “You are(answer)". 
+            Console.WriteLine("Hello, how are yuo?");
+            string answer = Console.ReadLine();
+            Console.WriteLine("You are " + answer);
+
+            //Task 1.3
+            //Read 3 variables of char type.
+            //Write message: “You enter (first char), (second char), (3 char)”
+            Console.WriteLine("Enter three Char");
+            char firstChar = Convert.ToChar(Console.ReadLine());
+            char secondChar = Convert.ToChar(Console.ReadLine());
+            char thirdChar = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("You enter {0},{1},{2}", firstChar, secondChar, thirdChar);
+            
+            //Task 1.4  
+            //Enter 2 integer numbers. Check if they are both positive – use bool expretion 
+            Console.WriteLine("Enter two int");
+            int firstIntNumber = Convert.ToInt32(Console.ReadLine());
+            int secondIntNumber = Convert.ToInt32(Console.ReadLine());
+            if (firstIntNumber > 0 & secondIntNumber > 0)
+            {
+                Console.WriteLine("they are both positive");
+            }
+            else
+            {
+                Console.WriteLine("they are both negative");
+            }
+
+            
         }
     }
 }
