8cff6ed510a6125bc00003e658e2d145fad9c550
TarasHrysiukk <64036257+TarasHrysiukk@users.noreply.github.com>
2021-05-12 16:52:01 +0300

    Add_8Task_and_Individual_Task

---
+++ Individual_Task_V4/Individual_Task_V4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Individual_Task_V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class Animal
+        {
+            DateTime birthYear;
+            DateTime curentYear = DateTime.Today;
+            string color;
+
+            public DateTime BirthYear { get; set; }
+            public string Color { get; set; }
+
+            public Animal(DateTime birthYearAnimal, String colorAnimal)
+            {
+                BirthYear = birthYearAnimal;
+                Color = colorAnimal;
+            }
+            string Voice()
+            {
+                return "I am an animal!";
+            }
+
+            public void Input()
+            {
+                Console.WriteLine("Enter color of your animal: ");
+                string yourAnimalColor = Console.ReadLine();
+                Console.WriteLine("Enter date of birth this animal");
+                DateTime birthDateYouAnimal = Convert.ToDateTime(Console.ReadLine());
+                BirthYear = birthDateYouAnimal;
+               Color = yourAnimalColor;
+            }
+
+            public void Output()
+            {
+                Console.WriteLine($"{Color} animal was born in {BirthYear}");
+            }
+
+            public void GetAge()
+            {
+                //int age = 
+            }
+        }
+    }
+}
--- Presentation8/Task_Presentation8/Task_Presentation8/Program.cs
+++ Presentation8/Task_Presentation8/Task_Presentation8/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace Task_Presentation8
 {
@@ -6,7 +10,119 @@ namespace Task_Presentation8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Person> staff = new List<Person>() ;
+            staff.Add(new Staff("Taras", 250));
+            staff.Add(new Staff("Ihor", 2000));
+            staff.Add(new Staff("oleh", 2000));
+            staff.Add(new Staff("Dasha", 250));
+            staff.Add(new Teacher("Vlad", 400, "C#"));
+            staff.Add(new Teacher("Vova", 450, "Java"));
+            staff.Add(new Teacher("Mykola", 500, "Ruby"));
+            staff.Add(new Developer("Tania", 700, "Midle"));
+            staff.Add(new Developer("Severyn", 750, "Junior"));
+            staff.Add(new Developer("Andrew", 750, "Senior"));
+
+           // staff.Sort();
+            foreach (var s in staff)
+            {
+                s.Print();
+            }
+            Console.WriteLine("Enter randome Persone name");
+            string personeName = Console.ReadLine();
+            int count = 0;
+            foreach (var s in staff)
+            {
+                
+                if (s.Name.Contains(personeName))
+                {
+                    Console.WriteLine("This Person is present in Staff:");
+                    s.Print();
+                    break;
+                }
+                else if((staff.Count).Equals(count+1))
+                {
+                    Console.WriteLine("This Person is not in the staff");
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            
+
+           
+        }
+
+
+        public class Person
+        {
+            private string name;
+            public Person(string name)
+            {
+                this.name = name;
+            }
+            virtual public string Name { get { return name; } }
+
+            virtual public void Print()
+            {
+                Console.WriteLine($"Name : { this.name}");
+            }
+        }
+
+
+        public class Staff : Person
+        {
+            private int salary;
+            virtual public int Salary { get { return salary; } }
+            public Staff(string name, int salary) : base(name)
+            {
+                this.salary = salary;
+            }
+            public override string Name { get { return base.Name + " -Staff"; } }
+
+            override public void Print()
+            {
+                Console.WriteLine($"Person {Name} has salary {this.salary}");
+            }
+            
+        }
+
+
+        public class Teacher : Staff
+        {
+            private string subject;
+            virtual public string Subject{ get { return subject; } }
+
+            public Teacher(string name, int salary, string subject) : base(name,salary)
+            {
+                this.subject = subject;
+            }
+
+            public override string Name { get { return base.Name + " -Teacher"; } }
+            public override void Print()
+            {
+                Console.WriteLine($"Teacher{Name} teach {subject} subject has salary {Salary} ");
+            }
+
+        }
+
+
+        public class Developer : Staff
+        {
+            private string level;
+            virtual public string Level { get { return level; } }
+
+            public Developer(string name, int salary, string level) : base(name, salary)
+            {
+                this.level = level;
+            }
+
+            override public string Name { get { return base.Name + " -Developer"; } }
+            public override void Print()
+            {
+                Console.WriteLine($"Developer{Name} hi is {Level} and has salary {Salary}");
+            }
+
         }
     }
 }
