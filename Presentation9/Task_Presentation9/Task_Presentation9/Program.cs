06dab519a41968981c9881ea9990458570e7204f
TarasHrysiukk <64036257+TarasHrysiukk@users.noreply.github.com>
2021-05-13 15:47:53 +0300

    Task_Presentation9_Done

---
+++ Presentation9/Task_Presentation9/Task_Presentation9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_Presentation9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = -1; i < 9; i++)
+            {
+                numbers.Add(i);
+                Console.Write($"{i}, ");
+            }
+
+            Console.WriteLine("\nNegative numbers: ");
+            var underZero = string.Join(", ", numbers.Where(item => item < 0));
+            Console.WriteLine($"{underZero}");
+
+            Console.WriteLine("Positive numbers: ");
+            var aboveZero = string.Join(", ", numbers.Where(item => item >= 0));
+            Console.WriteLine($"{aboveZero}");
+
+            int max = numbers.Max();
+            int min = numbers.Min();
+            Console.WriteLine($"Largest number: {max}, Smallest: {min}");
+
+            double average = numbers.Average();
+            Console.WriteLine($"Average: {average}");
+            var underAverage = string.Join(", ", numbers.Where(item => item < average));
+            Console.WriteLine($"Integers are above average: {underAverage}");
+
+            Console.WriteLine($"Largest one: {underAverage.Max()}");
+
+            var ordered_numbers = string.Join(",", numbers.OrderBy(nums => nums));
+            Console.WriteLine($"Ordered by ASC: {ordered_numbers}");
+        }
+    }
+}
